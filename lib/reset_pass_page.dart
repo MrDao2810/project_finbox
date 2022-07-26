36859c417616c9ed092b327bab338e398900bedf
CodeBro <daoquyen833@gmail.com>
2022-07-26 16:12:26 +0700

    Create function

--- lib/reset_pass_page.dart
+++ lib/reset_pass_page.dart
@@ -33,158 +33,166 @@ class _MyHomePageState extends State<ResetPassPage> {
   bool isValid = false;
   // String pattern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
   RegExp regex = RegExp(r'(^0{1}8{1}[0-9]{8}$)');
+
+  Widget _generateTitleSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.only(left: 2, bottom: 10),
+          child: const Text(
+            'Đặt lại mật khẩu',
+            style: TextStyle(
+              color: Color(0xff1B447E),
+              fontFamily: "Inter",
+              fontSize: 28.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: const Text(
+            'Nhập số điện thoại của bạn.',
+            style: TextStyle(
+              color: Color(0xff1B447E),
+              fontFamily: "Inter",
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        Container(
+          width: 42,
+          height: 3,
+          decoration: const BoxDecoration(
+            color: Color(0xff3563A3),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _generateFormSection() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 32, bottom: 14),
+            child: RichText(
+              text: const TextSpan(
+                text: 'SĐT',
+                style: TextStyle(
+                  color: Color(0xff999999),
+                  fontFamily: "Roboto",
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: '*', style: TextStyle(color: Color(0xffF85464))),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            child: TextFormField(
+              onChanged: (text) {
+                if (_formKey.currentState == null ||
+                    !_formKey.currentState!.validate()) {
+                  setState(() {
+                    isValid = false;
+                  });
+                } else {
+                  setState(() {
+                    isValid = true;
+                  });
+                }
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                ),
+                contentPadding: const EdgeInsets.only(left: 30, top: 18),
+                hintStyle: const TextStyle(
+                    color: Color(0xffCCCFD4),
+                    fontSize: 14,
+                    fontFamily: "Inter"),
+                hintText: 'Nhập Số điện thoại của bạn',
+                // labelText: 'Number Phone ',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty || !regex.hasMatch(value)) {
+                  if (isValid) {
+                    setState(() {
+                      isValid = false;
+                    });
+                  }
+                  return 'Please enter number phone';
+                } else {
+                  if (!isValid && regex.hasMatch(value)) {
+                    setState(() {
+                      isValid = true;
+                    });
+                  }
+                  return null;
+                }
+              },
+            ),
+          ),
+          Align(
+            child: Container(
+              margin: const EdgeInsets.only(top: 30, bottom: 30),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: isValid
+                    ? () {
+                        // you'd often call a server or save the information in a database.
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const PassPageOtp()),
+                        );
+                      }
+                    : null,
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    backgroundColor:
+                        MaterialStateProperty.all(const Color(0xff1E60C0)),
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(25)),
+                    textStyle: MaterialStateProperty.all(
+                        const TextStyle(fontSize: 16))),
+                child: const Text('XÁC NHẬN'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: const Text('Đặt lại mật khẩu'),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: const Text('Đặt lại mật khẩu'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(top: 30, left: 15, right: 15),
+        child: ListView(
+          children: [
+            _generateTitleSection(),
+            _generateFormSection()
+          ],
         ),
-        body: Container(
-          margin: const EdgeInsets.only(top: 30, left: 15, right: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(left: 2, bottom: 10),
-                child: const Text(
-                  'Đặt lại mật khẩu',
-                  style: TextStyle(
-                    color: Color(0xff1B447E),
-                    fontFamily: "Inter",
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: const Text(
-                  'Nhập số điện thoại của bạn.',
-                  style: TextStyle(
-                    color: Color(0xff1B447E),
-                    fontFamily: "Inter",
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              Container(
-                width: 42,
-                height: 3,
-                decoration: const BoxDecoration(
-                  color: Color(0xff3563A3),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 32, bottom: 14),
-                child: RichText(
-                  text: const TextSpan(
-                    text: 'SĐT',
-                    style: TextStyle(
-                      color: Color(0xff999999),
-                      fontFamily: "Roboto",
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: '*',
-                          style: TextStyle(color: Color(0xffF85464))),
-                    ],
-                  ),
-                ),
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    Container(
-                      child: TextFormField(
-                        onChanged: (text) {
-                          if (_formKey.currentState == null ||
-                              !_formKey.currentState!.validate()) {
-                            setState(() {
-                              isValid = false;
-                            });
-                          } else {
-                            setState(() {
-                              isValid = true;
-                            });
-                          }
-                        },
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(6.0),
-                          ),
-                          contentPadding:
-                              const EdgeInsets.only(left: 30, top: 18),
-                          hintStyle: const TextStyle(
-                              color: Color(0xffCCCFD4),
-                              fontSize: 14,
-                              fontFamily: "Inter"),
-                          hintText: 'Nhập Số điện thoại của bạn',
-                          // labelText: 'Number Phone ',
-                        ),
-                        validator: (value) {
-                          if (value == null ||
-                              value.isEmpty ||
-                              !regex.hasMatch(value)) {
-                            if (isValid) {
-                              setState(() {
-                                isValid = false;
-                              });
-                            }
-                            return 'Please enter number phone';
-                          } else {
-                            if (!isValid && regex.hasMatch(value)) {
-                              setState(() {
-                                isValid = true;
-                              });
-                            }
-                            return null;
-                          }
-                        },
-                      ),
-                    ),
-                    Align(
-                      child: Container(
-                        margin: const EdgeInsets.only(top: 30, bottom: 30),
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: isValid
-                              ? () {
-                                  // you'd often call a server or save the information in a database.
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const PassPageOtp()),
-                                  );
-                                }
-                              : null,
-                          style: ButtonStyle(
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10))),
-                              backgroundColor: MaterialStateProperty.all(
-                                  const Color(0xff1E60C0)),
-                              padding: MaterialStateProperty.all(
-                                  const EdgeInsets.all(25)),
-                              textStyle: MaterialStateProperty.all(
-                                  const TextStyle(fontSize: 16))),
-                          child: const Text('XÁC NHẬN'),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+      ),
+    ); // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
--- lib/reset_pass_page_otp.dart
+++ lib/reset_pass_page_otp.dart
@@ -8,6 +8,132 @@ class PassPageOtp extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<PassPageOtp> {
+
+  Widget _generateTitleSectionOtp(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.only(left: 2, bottom: 10),
+          child: const Text(
+            'Đặt lại mật khẩu',
+            style: TextStyle(
+              color: Color(0xff1B447E),
+              fontFamily: "Inter",
+              fontSize: 28.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.only(bottom: 32),
+          child: const Text(
+            'Nhập mã OTP đã được gửi đến số điện thoại \n *******112',
+            style: TextStyle(
+              color: Color(0xff999999),
+              fontFamily: "Inter",
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _generateFormSectionOtp(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.only(bottom: 14),
+          child: const Text(
+            'Mã xác minh SĐT',
+            style: TextStyle(
+              color: Color(0xff999999),
+              fontFamily: "Inter",
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        Container(
+          color: const Color(0xffF7F4F4),
+          height: 45,
+          child: TextField(
+            textAlign: TextAlign.justify,
+            decoration: InputDecoration(
+              enabledBorder: const OutlineInputBorder(
+                borderSide:
+                BorderSide(width: 1, color: Color(0xffF7F4F4)),
+              ),
+              suffixIcon:
+              Row(mainAxisSize: MainAxisSize.min, // <-- important
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(right: 17),
+                      child: const Text(
+                        'Gửi lại mã',
+                        style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xff1B447E)),
+                        //const SizedBox(width: 12.65),
+                        // second element
+                      ),
+                    )
+                  ]),
+            ),
+          ),
+        ),
+        Align(
+          child: Container(
+            margin: const EdgeInsets.only(top: 28),
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  shape:
+                  MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  backgroundColor:
+                  MaterialStateProperty.all(const Color(0xff1E60C0)),
+                  padding:
+                  MaterialStateProperty.all(const EdgeInsets.all(25)),
+                  textStyle: MaterialStateProperty.all(
+                      const TextStyle(fontSize: 16))),
+              child: const Text('XÁC NHẬN'),
+            ),
+          ),
+        ),
+        Container(
+            padding: const EdgeInsets.only(top: 22),
+            child: Center(
+              child: RichText(
+                text: const TextSpan(
+                  text: 'Bạn không nhận được mã? \n   Liên hệ trợ giúp ',
+                  style: TextStyle(
+                    color: Color(0xff000000),
+                    fontFamily: "Roboto",
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Tại đây',
+                        style: TextStyle(
+                            color: Color(0xff1E60C0), fontFamily: "Inter")),
+                  ],
+                ),
+              ),
+            )
+        ),
+      ],
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,124 +142,15 @@ class _MyHomePageState extends State<PassPageOtp> {
           // the App.build method, and use it to set our appbar title.
           title: const Text('Đặt lại mật khẩu'),
         ),
-        body: Container(
-          margin: const EdgeInsets.only(top: 30, left: 15, right: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(left: 2, bottom: 10),
-                child: const Text(
-                  'Đặt lại mật khẩu',
-                  style: TextStyle(
-                    color: Color(0xff1B447E),
-                    fontFamily: "Inter",
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(bottom: 32),
-                child: const Text(
-                  'Nhập mã OTP đã được gửi đến số điện thoại \n *******112',
-                  style: TextStyle(
-                    color: Color(0xff999999),
-                    fontFamily: "Inter",
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(bottom: 14),
-                child: const Text(
-                  'Mã xác minh SĐT',
-                  style: TextStyle(
-                    color: Color(0xff999999),
-                    fontFamily: "Inter",
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              Container(
-                color: const Color(0xffF7F4F4),
-                height: 45,
-                child: TextField(
-                  textAlign: TextAlign.justify,
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide:
-                          BorderSide(width: 1, color: Color(0xffF7F4F4)),
-                    ),
-                    suffixIcon:
-                        Row(mainAxisSize: MainAxisSize.min, // <-- important
-                            children: [
-                          Container(
-                            padding: const EdgeInsets.only(right: 17),
-                            child: const Text(
-                              'Gửi lại mã',
-                              style: TextStyle(
-                                  fontFamily: 'Inter',
-                                  fontStyle: FontStyle.normal,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color(0xff1B447E)),
-                              //const SizedBox(width: 12.65),
-                              // second element
-                            ),
-                          )
-                        ]),
-                  ),
-                ),
-              ),
-              Align(
-                child: Container(
-                  margin: const EdgeInsets.only(top: 28),
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10))),
-                        backgroundColor:
-                            MaterialStateProperty.all(const Color(0xff1E60C0)),
-                        padding:
-                            MaterialStateProperty.all(const EdgeInsets.all(25)),
-                        textStyle: MaterialStateProperty.all(
-                            const TextStyle(fontSize: 16))),
-                    child: const Text('XÁC NHẬN'),
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 22),
-                child: Center(
-                  child: RichText(
-                    text: const TextSpan(
-                      text: 'Bạn không nhận được mã? \n   Liên hệ trợ giúp ',
-                      style: TextStyle(
-                        color: Color(0xff000000),
-                        fontFamily: "Roboto",
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w400,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: 'Tại đây',
-                            style: TextStyle(
-                                color: Color(0xff1E60C0), fontFamily: "Inter")),
-                      ],
-                    ),
-                  ),
-                )
-              ),
-            ],
-          ),
-        ) // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        margin: const EdgeInsets.only(top: 30, left: 15, right: 15),
+        child: ListView(
+          children: [
+            _generateTitleSectionOtp(),
+            _generateFormSectionOtp()
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
         );
   }
 }
